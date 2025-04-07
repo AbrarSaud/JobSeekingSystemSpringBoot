f586a59b2f647eb77a00daf033a1fca1b6eb37b0
AbrarSaud <abrar.sauod0@gmail.com>
2025-04-07 14:38:58 +0300

    DONE

--- src/main/java/com/example/jobseekingsystem/Service/JobApplicationService.java
+++ src/main/java/com/example/jobseekingsystem/Service/JobApplicationService.java
@@ -2,8 +2,11 @@ package com.example.jobseekingsystem.Service;
 
 
 import com.example.jobseekingsystem.Model.JobApplication;
+import com.example.jobseekingsystem.Model.JobPost;
 import com.example.jobseekingsystem.Model.User;
 import com.example.jobseekingsystem.Repository.JobApplicationRepository;
+import com.example.jobseekingsystem.Repository.JobPostRepository;
+import com.example.jobseekingsystem.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class JobApplicationService {
-    public final JobApplicationRepository jobApplicationRepository ;
+    private final JobApplicationRepository jobApplicationRepository;
+    private final UserRepository userRepository;
+    private final JobPostRepository jobPostRepository;
 
 
     // Get all JobApplication
@@ -22,13 +27,19 @@ public class JobApplicationService {
 
     // Add a new JobApplication
     public void addJobApplication(JobApplication jobApplication) {
-        jobApplicationRepository.save(jobApplication);
+        User user = userRepository.getById(jobApplication.getUserId());
+        JobPost jobPost = jobPostRepository.getById(jobApplication.getJobPostId());
+        if (user != null && jobPost != null) {
+            jobApplicationRepository.save(jobApplication);
+        }
     }
 
     // Delete a JobApplication
     public Boolean deleteJobApplication(Integer id) {
         JobApplication jobApplication = jobApplicationRepository.getById(id);
-        if (jobApplication == null) {
+        User user = userRepository.getById(jobApplication.getUserId());
+        JobPost jobPost = jobPostRepository.getById(jobApplication.getJobPostId());
+        if (jobApplication == null && user == null && jobPost == null) {
             return false;
         }
         jobApplicationRepository.delete(jobApplication);
